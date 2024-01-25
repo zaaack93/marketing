4ebb5d6ab55b2a39e911259147fe39f493707515
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-01-25 15:25:47 +0100

    workign on automation

--- app/routes/app.automations.tsx
+++ app/routes/app.automations.tsx
@@ -1,8 +1,33 @@
+import { ActionFunction } from '@remix-run/node'
 import { Page } from '@shopify/polaris'
 import React from 'react'
+import { authenticate } from '~/shopify.server'
 
 type Props = {}
 //send automation email when user is created
+
+// 1 setup webhook
+// create automation
+
+export const action: ActionFunction = async ({ request }) => {
+
+  //TRIGGER WEBHOOK
+
+  const {admin,session} = await authenticate.admin(request)
+  const webhook = new admin.rest.resources.Webhook({session})
+  ///create a webhook on the system
+  if(webhook){
+    console.log(webhook,"-------------webhook hit create user----------------")
+    webhook.address=""
+    webhook.topic="customers/create"
+    webhook.format="json"
+    console.log("-------------webhook succesfully created----------------")
+    await webhook.save({update:true})
+  
+  }
+
+  return null
+} 
 
 const AutomationsPage = (props: Props) => {
   return (
--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,6 +1,10 @@
-import type { ActionFunctionArgs } from "@remix-run/node";
+import { json, type ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
+import { EmailNew } from "~/emails/New";
+import { Resend } from "resend";
+
+const resend = new Resend('re_6SrMmFee_KoPqFLvS4Ft3njNzrNCXNb1r')
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { topic, shop, session, admin, payload } = await authenticate.webhook(
@@ -21,8 +25,21 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       break;
 
     case "CUSTOMERS_CREATE":
-     
-  
+      console.log('---------hit customers webhook---------')
+      console.log(payload)
+
+
+      const { data,error } = await resend.emails.send({
+        from: 'REMIX HOOK <onboarding@resend.dev>',
+        to : ['bouzangadzakaria@gmail.com'],
+        subject: 'hello youtube',
+        react: <EmailNew  url={''}/>
+      })
+      if(error){
+        return json({error},400)
+      }
+    
+      return json({data},200)
       break; 
     case "CUSTOMERS_DATA_REQUEST":
     case "CUSTOMERS_REDACT":
